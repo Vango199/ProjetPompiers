4d67c53800e9c994539993a9c29da64885c05fe2
Vango199 <62698943+Vango199@users.noreply.github.com>
2021-06-01 23:58:10 +0200

    Requetes de base vers FireSimu

---
+++ src/main/java/com/sp/rest/SimulationCrtrl.java
@@ -0,0 +1,51 @@
+package com.sp.rest;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.project.model.dto.FireDto;
+
+
+
+
+@RestController
+@RequestMapping("/simulation")
+
+public class SimulationCrtrl {
+	@Autowired
+
+	@Autowired
+	
+    
+    @RequestMapping(method=RequestMethod.POST,value="/test")
+    public void test1() {
+        System.out.println("okokokokok");
+    }
+	
+	
+    @GetMapping("/test")
+    public String test() {
+        return "hello";
+    }
+	
+    @RequestMapping(method=RequestMethod.GET,value="/fire") 
+	public List<FireDto> GetFires(HttpServletResponse response,HttpServletRequest request) {
+	  
+		 
+		return mService.buyACard(buyer, card, market);
+		  
+	  
+	
+}
+
---
+++ src/main/java/com/sp/service/SimulationService.java
@@ -0,0 +1,55 @@
+package com.sp.service;
+
+import java.util.List;
+
+import org.springframework.web.client.RestTemplate;
+
+import com.project.model.dto.FireDto;
+import com.project.model.dto.VehicleDto;
+
+
+public class SimulationService {
+
+	public List<FireDto> getFire() {
+		String UrlGetFires = "http://127.0.0.1:8081/fire";
+		List<FireDto> ListFire = new RestTemplate().getForObject(UrlGetFires,List.class);//pas sûr que ca va marcher
+		return ListFire;
+	}
+	
+	public void resetFire() {
+		String UrlResetFires = "http://127.0.0.1:8081/fire/reset";
+		new RestTemplate().put( UrlResetFires, null);
+		return ;
+	}
+	
+	public void PostVehicle(VehicleDto _vehicle) {
+		String UrlPostVehicle = "http://127.0.0.1:8081/vehicle";
+		new RestTemplate().postForEntity(UrlPostVehicle, _vehicle, null);
+		return ;
+	}
+	
+	public List<VehicleDto> GetVehicle() {
+		String UrlGetVehicle = "http://127.0.0.1:8081/vehicle";
+		List<VehicleDto> listVehicle = new RestTemplate().getForObject(UrlGetVehicle, List.class );
+		return listVehicle;
+	}
+	
+	public VehicleDto GetVehicleById(Integer _vehicleId) {
+		String UrlGetVehicle = "http://127.0.0.1:8081/vehicle/"+_vehicleId;
+		VehicleDto vehicle = new RestTemplate().getForObject(UrlGetVehicle, VehicleDto.class );
+		return vehicle;
+	}
+	
+	public void PutVehicle( VehicleDto _vehicle) {
+		String UrlPutVehicle = "http://127.0.0.1:8081/vehicle/"+_vehicle.getId();
+		new RestTemplate().put(UrlPutVehicle, _vehicle );
+		return ;
+	}
+	
+	public void DeleteVehicle( VehicleDto _vehicle) {
+		String UrlDeleteVehicle = "http://127.0.0.1:8081/vehicle/"+_vehicle.getId();
+		new RestTemplate().delete(UrlDeleteVehicle);
+		return ;
+	}
+	
+}
