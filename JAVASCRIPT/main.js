a9d2b711e4d1a7cc8e5b90657a59c7d8c142c075
gab-dm <gabm19@gmail.com>
2021-06-04 11:15:33 +0200

    Affichage/création des Camions

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -85,7 +85,7 @@ function init() {
     L.control.layers(null,overlayMaps_type).addTo(map);
 
   GetAllFire();
-  GetAllCamionsBomberos();
+  //GetAllCamionsBomberos();
 }
 
 
@@ -247,7 +247,7 @@ function AffichageDonneeFeux(Feux){ //Affichage des données liées au feu
 
 
 ////////////////////////////////////////////////////// AFFICHAGE DES CAMIONS DE BOMBEROS /////////////////////////////////////////////////
-
+/*
 function GetAllCamionsBomberos(){ //appel de la liste avec tous les camions de pompier
   fetch('http://localhost:8081/vehicle')
   .then(
@@ -273,15 +273,13 @@ function GetAllCamionsBomberos(){ //appel de la liste avec tous les camions de p
 
 function AffichageCamions(AllCamionsBomberosList){
 
+    for (i in AllCamionsBomberosList){//On parcourt la liste des feux pour venir les afficher sur la map
 
-
-for (i in AllCamionsBomberosList){//On parcourt la liste des feux pour venir les afficher sur la map
-
-var myIcon = L.icon({
-  iconUrl: '../Img/my-icon.png',
-  iconSize: [30,  20],
-  iconAnchor: [29, 19],
-});
+    var myIcon = L.icon({
+    iconUrl: '../Img/my-icon.png',
+    iconSize: [30,  20],
+    iconAnchor: [29, 19],
+    });
 
 var my_marker =L.marker([AllCamionsBomberosList[i].lat, AllCamionsBomberosList[i].lon], {icon: myIcon});
 my_marker.bindPopup(AffichageDonneeCamionsBomberos(AllCamionsBomberosList[i]));
@@ -294,8 +292,165 @@ function AffichageDonneeCamionsBomberos(Camion){ //Affichage des données liées
   y = '<p>' + '<img src="../Img/my-icon.png" width="25" height="25" />' + '<b>  Camion de Pompier : </b>'+ '<br />' + 'Id : ' + Camion.id+ '<br />' + 'Type : ' + Camion.type+ '<br />' + 'Capacité : ' + Camion.crewMemberCapacity + '<br />' + 'Fuel : : ' + Camion.fuel;
   return y.toString()
 }
-
+*/
 ////////////////////////////////////////////////////// AFFICHAGE DES CAMIONS DE BOMBEROS /////////////////////////////////////////////////
+var FiretruckIcon = new L.Icon({
+    iconUrl: '../Img/my-icon.png',
+    iconSize:     [30, 30],
+    iconAnchor: new L.Point(16, 16),
+    });
+var CarIcon = new L.Icon({
+    iconUrl: '../Img/my-icon.png',
+    iconSize:     [30, 30],
+    iconAnchor: new L.Point(16, 16),
+    });
+var PumperTruckIcon = new L.Icon({
+    iconUrl: '../Img/my-icon.png',
+    iconSize:     [30, 30],
+    iconAnchor: new L.Point(16, 16),
+    });
+var WaterTenderIcon = new L.Icon({
+    iconUrl: '../Img/my-icon.png',
+    iconSize:     [30, 30],
+    iconAnchor: new L.Point(16, 16),
+    });
+var LadderTruckIcon = new L.Icon({
+    iconUrl: '../Img/my-icon.png',
+    iconSize:     [30, 30],
+    iconAnchor: new L.Point(16, 16),
+    });    
+var TruckIcon = new L.Icon({
+    iconUrl: '../Img/my-icon.png',
+    iconSize:     [30, 30],
+    iconAnchor: new L.Point(16, 16),
+    });
+
+
+function displayInfoVehiculeMap(){
+    const context = {
+        method: 'GET'
+    }
+    fetch('http://localhost:8081/vehicle', context)
+        .then(response => response.json().then(body => displayVehicle(body)))
+        .catch(error => console.log(error))
+        setTimeout(displayInfoVehiculeMap, TempsdeRefresh);
+}
+
+function displayVehicle(body){
+
+            mapIdVehiculeLayerNew = new Map();
+            
+            for(const vehicle of body){
+                var Vehicle = L.marker([vehicle.lat, vehicle.lon], {
+                    icon: FiretruckIcon
+                }).addTo(map);
+                Vehicle.bindPopup("Id : " + vehicle.id + "<br>Type : " + vehicle.type + "<br>Liquid Load : " + vehicle.liquidType +" " +vehicle.liquidQuantity + "L<br>" + "Fuel : "+ vehicle.fuel+ "<br><button type='button' id="+vehicle.id+" onclick=deleteVehicle(this.id)>Supprimer</button>");
+                
+
+                if(vehicle.type == "CAR"){
+                    Vehicle.setIcon(CarIcon);
+                }
+                if(vehicle.type == "FIRE_ENGINE"){
+                    Vehicle.setIcon(FiretruckIcon);
+                }
+                if(vehicle.type == "PUMPER_TRUCK"){
+                    Vehicle.setIcon(PumperTruckIcon);
+                }
+                if(vehicle.type == "WATER_TENDER"){
+                    Vehicle.setIcon(WaterTenderIcon);
+                }
+                if(vehicle.type == "TURNTABLE_LADDER_TRUCK"){
+                    Vehicle.setIcon(LadderTruckIcon);
+                }
+                if(vehicle.type == "TRUCK"){
+                    Vehicle.setIcon(TruckIcon);
+                }
+                
+            }
+}
+
+function deleteVehicle(id){
+    const context = {
+        method: 'DELETE'
+    }
+    fetch('http://localhost:8081/vehicle/'+id, context)
+}
+
+TruckTypeSelect = '<label for="TruckType">Choose a type of car :<br></label><select id="TruckType" name="TruckType"><option value="CAR">CAR</option><option value="WATER_TENDERS">WATER_TENDERS</option><option value="TURNTABLE_LADDER_TRUCK">TURNTABLE_LADDER_TRUCK</option><option value="TRUCK">TRUCK</option><option value="FIRE_ENGINE">FIRE_ENGINE</option><option value="PUMPER_TRUCK">PUMPER_TRUCK</option></select>';    
+LiquidTypeSelect = '<label for="LiquidType">Choose a type of liquid :</label><select id="LiquidType" name="LiquidType"><option value="ALL">ALL</option><option value="WATER">WATER</option><option value="WATER_WITH_ADDITIVES">WATER_WITH_ADDITIVES</option><option value="CARBON_DIOXIDE">CARBON_DIOXIDE</option><option value="POWDER">POWDER</option></select>';
+var AddPopup = L.popup().setContent('Define what vehicule you desire<br><form onsubmit=AddVehicle(event) method="POST" id="AddVehicle">'+TruckTypeSelect+'<br>'+LiquidTypeSelect+'<br><input type="submit"></form>');
+
+AddButton = L.easyButton('<img title = "Add a vehicle" src="../Img/my-icon.png">', function(btn, imap){
+
+AddPopup.setLatLng(imap.getCenter()).openOn(imap); 
+
+}).addTo(map);
+
+
+        
+function AddVehicle(event){
+    event.preventDefault();
+    var DataVehicule = document.getElementById("AddVehicule");
+    var charge = {
+        
+        "lon": 4.228066,
+        "lat": 45.747389,
+        "type": document.getElementById('TruckType').value,
+        "efficiency": 10.0,
+        "liquidType": document.getElementById('LiquidType').value,
+        "liquidQuantity": 100.0,
+        "liquidConsumption": 1.0,
+        "fuel": 100.0,
+        "fuelConsumption": 10.0,
+        "crewMember": 8,
+        "crewMemberCapacity": 8,
+        "facilityRefID": 0
+    }
+    console.log(charge);
+    const context = {
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json'
+            },
+        body: JSON.stringify( charge ) 
+    }
+    fetch("http://localhost:8081/vehicle", context)
+        
+}
+
+function UpdateVehicle(event){
+    event.preventDefault();
+    var DataVehicule = document.getElementById("AddVehicule");
+    var charge = {
+        
+        "lon": 4.828066,
+        "lat": 45.747389,
+        "type": document.getElementById('TruckType').value,
+        "efficiency": 10.0,
+        "liquidType": document.getElementById('LiquidType').value,
+        "liquidQuantity": 100.0,
+        "liquidConsumption": 1.0,
+        "fuel": 100.0,
+        "fuelConsumption": 10.0,
+        "crewMember": 8,
+        "crewMemberCapacity": 8,
+        "facilityRefID": 0
+    }
+    console.log(charge);
+    const context = {
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json'
+            },
+        body: JSON.stringify( charge ) 
+    }
+    fetch("http://localhost:8081/vehicle", context)
+        
+}
+
+
+    displayInfoVehiculeMap();
+
 /*
 function RecupVehicleIncendie(){ //récupère le véhicule d'urgence avec en plus un Id du feu
     fetch('http://localhost:8082/vehicle/getall')
@@ -340,13 +495,14 @@ function RecupFireFromidFire(){//récupère à partir de l'Id du feu, l'objet fe
     console.log('Fetch Error :-S', err);
   });
 }
+*/
 
-function TracerItineraire(){
+//function TracerItineraire(){
   //LatDep = RecupVehicleIncendie().lat
   //LonDep = RecupVehicleIncendie().lon
   //LatAriv = RecupFireFromidFire().lat
   //LonAriv = RecupFireFromidFire().lon
-*/
+
 
 /*
   var latlngs = [
@@ -1046,6 +1202,5 @@ coords = [
   }
 
 displayJourneyReshaped(coords);
-
 
 
