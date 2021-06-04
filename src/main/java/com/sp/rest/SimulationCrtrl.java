82fea2157cbec452304db0275e4fb341cccb8ae9
Thomas Scalise <thomas.scalise@hotmail.fr>
2021-06-04 14:53:17 +0200

    kjpk

--- src/main/java/com/sp/rest/SimulationCrtrl.java
+++ src/main/java/com/sp/rest/SimulationCrtrl.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +21,7 @@ import com.sp.service.FireSimulationService;
 
 
 
+@CrossOrigin
 @RestController
 @RequestMapping("/simulation")
 
