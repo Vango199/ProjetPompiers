191fae887766ff89b067e95a43462701274d9960
Thomas Scalise <thomas.scalise@hotmail.fr>
2021-06-04 16:26:04 +0200

    ON CREER DES CAMIONS

--- JAVASCRIPT/camion.js
+++ JAVASCRIPT/camion.js
@@ -1,52 +1,4 @@
-/* 
-function GetAllCamionsBomberos(){ //appel de la liste avec tous les camions de pompier
-    fetch('http://localhost:8081/vehicle')
-    .then(
-      function(response) {
-        if (response.status !== 200) {
-          console.log('Looks like there was a problem. Status Code: ' +
-            response.status);
-          return;
-        }
-  
-        // Examine the text in the response
-        response.json().then(function(data) {
-          AffichageCamions(data);
-          return data;
-        });
-      }
-    )
-    .catch(function(err) {
-      console.log('Fetch Error :-S', err);
-    });
-    setTimeout(GetAllCamionsBomberos, TempsdeRefresh);
-  }
-  
-  function AffichageCamions(AllCamionsBomberosList){
-  
-  
-  
-  for (i in AllCamionsBomberosList){//On parcourt la liste des feux pour venir les afficher sur la map
-  
-  var myIcon = L.icon({
-    iconUrl: '../Img/my-icon.png',
-    iconSize: [30,  20],
-    iconAnchor: [29, 19],
-  });
-  
-  var my_marker =L.marker([AllCamionsBomberosList[i].lat, AllCamionsBomberosList[i].lon], {icon: myIcon});
-  my_marker.bindPopup(AffichageDonneeCamionsBomberos(AllCamionsBomberosList[i]));
-  my_marker.addTo(pompier);
-  pompier.addTo(map);
-  }
-  }
-  
-  function AffichageDonneeCamionsBomberos(Camion){ //Affichage des données liées au feu
-    y = '<p>' + '<img src="../Img/my-icon.png" width="25" height="25" />' + '<b>  Camion de Pompier : </b>'+ '<br />' + 'Id : ' + Camion.id+ '<br />' + 'Type : ' + Camion.type+ '<br />' + 'Capacité : ' + Camion.crewMemberCapacity + '<br />' + 'Fuel : : ' + Camion.fuel;
-    return y.toString()
-  }
-*/
-  
+
 ////////////////////////////////////////////////////// AFFICHAGE DES CAMIONS DE BOMBEROS /////////////////////////////////////////////////
 
 
@@ -54,10 +6,35 @@ function displayInfoVehiculeMap(){
   const context = {
       method: 'GET'
   }
-  fetch('http://localhost:8081/vehicle', context)
+  fetch('http://localhost:8082/vehicle/getall', context)
       .then(response => response.json().then(body => displayVehicle(body)))
       .catch(error => console.log(error))
       setTimeout(displayInfoVehiculeMap, TempsdeRefresh);
+}
+
+
+function GetAllVehicle(){ //appel de la liste avec tous les feux
+  fetch('http://localhost:8082/vehicle/getall')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+          displayVehicle(data);
+     //   AffichageFeux(data);
+        return data;
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+  setTimeout(GetAllFire, TempsdeRefresh);
 }
 
 function displayVehicle(body){
@@ -68,7 +45,8 @@ function displayVehicle(body){
               var Vehicle = L.marker([vehicle.lat, vehicle.lon], {
                   icon: FiretruckIcon
               }).addTo(map);
-              Vehicle.bindPopup("Id : " + vehicle.id + "<br>Type : " + vehicle.type + "<br>Liquid Load : " + vehicle.liquidType +" " +vehicle.liquidQuantity + "L<br>" + "Fuel : "+ vehicle.fuel+ "<br><button type='button' id="+vehicle.id+' onclick=deleteVehicle(this.id)>Supprimer</button>');
+               Vehicle.bindPopup("Id : " + vehicle.id + "<br>Type : " + vehicle.type + "<br>Liquid Load : " + vehicle.liquidType + " " + vehicle.liquidQuantity + "L<br>" + "Fuel : " + vehicle.fuel + "<br><button type='button' id=" + vehicle.id + " onclick=deleteVehicle(this.id, this.lon, this.lat, this.type, this.efficiency, this.liqiudType, this.liquidQuantity, this.liquidConsumption, this.fuel, this.fuelConsumption, this.crewMember, this.crewMemberCapacity, this.facilityRefID)>Supprimer</button><button id=" + vehicle.id + " type='button' onclick=editVehicle(this.id)>Modifier</button>");
+
               
 
               if(vehicle.type == "CAR"){
@@ -93,12 +71,37 @@ function displayVehicle(body){
           }
 }
 
-function deleteVehicle(id){
-  const context = {
-      method: 'DELETE'
-  }
-  fetch('http://localhost:8081/vehicle/'+id, context)
+function deleteVehicle(id, lon, lat, type, efficiency, liquidType, liquidQuantity, liquidConsumption, fuel, fuelConsumption, crewMember, crewMemberCapacity, facilityRefID) {
+  mymap.closePopup();
+  var charge_delete = {
+    "id": id,
+    "lon": lon,
+    "lat": lat,
+    "type": document.getElementById('TruckType').value,
+    "efficiency": efficiency,
+    "liquidType": document.getElementById('LiquidType').value,
+    "liquidQuantity": liquidQuantity,
+    "liquidConsumption": liquidConsumption,
+    "fuel": fuel,
+    "fuelConsumption": fuelConsumption,
+    "crewMember": crewMemberCapacity,
+    "crewMemberCapacity": crewMemberCapacity,
+    "facilityRefID": facilityRefID
 }
+  const context = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+        },
+    body: JSON.stringify( charge_delete ) 
+  }
+  fetch('http://localhost:8082/vehicle/deletevehicle', context)
+      .then()
+      .catch(error => console.log(error))
+}
+
+
+
 
 TruckTypeSelect = '<label for="TruckType">Choose a type of car :<br></label><select id="TruckType" name="TruckType"><option value="CAR">CAR</option><option value="WATER_TENDERS">WATER_TENDERS</option><option value="TURNTABLE_LADDER_TRUCK">TURNTABLE_LADDER_TRUCK</option><option value="TRUCK">TRUCK</option><option value="FIRE_ENGINE">FIRE_ENGINE</option><option value="PUMPER_TRUCK">PUMPER_TRUCK</option></select>';    
 LiquidTypeSelect = '<label for="LiquidType">Choose a type of liquid :</label><select id="LiquidType" name="LiquidType"><option value="ALL">ALL</option><option value="WATER">WATER</option><option value="WATER_WITH_ADDITIVES">WATER_WITH_ADDITIVES</option><option value="CARBON_DIOXIDE">CARBON_DIOXIDE</option><option value="POWDER">POWDER</option></select>';
@@ -118,9 +121,9 @@ function AddVehicle(event){
   event.preventDefault();
   var DataVehicule = document.getElementById("AddVehicule");
   var charge = {
-      
-      "lon": 45.7, //document.getElementById('POST-lat'),
-      "lat": 4.85,//document.getElementById('POST-lon'),
+      "id": 40.0,
+      "lon": document.getElementById('POST-lon').value,
+      "lat": document.getElementById('POST-lat').value,
       "type": document.getElementById('TruckType').value,
       "efficiency": 10.0,
       "liquidType": document.getElementById('LiquidType').value,
@@ -140,39 +143,11 @@ function AddVehicle(event){
           },
       body: JSON.stringify( charge ) 
   }
-  fetch("http://localhost:8081/vehicle", context)
+  fetch("http://localhost:8082/vehicle", context)
       
 }
 
-function UpdateVehicle(event){
-  event.preventDefault();
-  var DataVehicule = document.getElementById("AddVehicule");
-  var charge = {
-      
-      "lon": 4.828066,
-      "lat": 45.747389,
-      "type": document.getElementById('TruckType').value,
-      "efficiency": 10.0,
-      "liquidType": document.getElementById('LiquidType').value,
-      "liquidQuantity": 100.0,
-      "liquidConsumption": 1.0,
-      "fuel": 100.0,
-      "fuelConsumption": 10.0,
-      "crewMember": 8,
-      "crewMemberCapacity": 8,
-      "facilityRefID": 0
-  }
-  console.log(charge);
-  const context = {
-      method:'POST',
-      headers: {
-          'Content-Type': 'application/json'
-          },
-      body: JSON.stringify( charge ) 
-  }
-  fetch("http://localhost:8081/vehicle", context)
-      
-}
+
 
 
   displayInfoVehiculeMap();
